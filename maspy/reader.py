94db0b4b2ee63f54c7672c6397270a6c6379eda8
David M. Hollenstein <hollenstein.david@gmail.com>
2016-04-13 16:37:38 +0200

    Added reader.py and writer.py, removed import_export.py

--- maspy/reader.py
+++ maspy/reader.py
@@ -7,25 +7,25 @@ except ImportError: # python 3.x series
     pass
 ###############################################################################
 import io
+from lxml import etree as ETREE
 import numpy
 import os
 
-from lxml import etree as ETREE
-
 import maspy.auxiliary as aux
-import maspy.xml
 import maspy.core
 import maspy.peptidemethods
+import maspy.xml
 
 
-##########################################################
-### mzml import and export methods #######################
-##########################################################
+#####################################################################
+### mzml import methods                       #######################
+#####################################################################
 def importMzml(filepath, msrunContainer=None, siAttrFromSmi=None, specfilename=None):
     """Performs a complete import of a mzml file into a maspy MsrunContainer.
 
     siAttrFromSmi: allow here to specify a custom function that extracts params a from spectrumMetadataItem
     """
+    #TODO: docstring
     siAttrFromSmi = defaultFetchSiAttrFromSmi if siAttrFromSmi is None else siAttrFromSmi
     if msrunContainer is None:
         msrunContainer = maspy.core.MsrunContainer()
@@ -83,94 +83,9 @@ def importMzml(filepath, msrunContainer=None, siAttrFromSmi=None, specfilename=N
     return msrunContainer
 
 
-def writeMzml(specfile, msrunContainer, outputdir, spectrumIds=None, chromatogramIds=None):
-    #TODO: maybe change to use aux.openSafeReplace
-    outputFile = io.BytesIO()
-
-    #TODO: perform check that specfile is present in msrunContainer and at least the metadatanode.
-    metadataTree = msrunContainer.rmc[specfile]
-    #Generate a list of spectrum ids that should be written to mzML
-    if spectrumIds is None and specfile in msrunContainer.smic:
-        keyTuple = [(int(key), key) for key in viewkeys(msrunContainer.smic[specfile])]
-        spectrumIds = [key for _, key in sorted(keyTuple)]
-    spectrumCounts = len(spectrumIds)
-    #Generate a list of chromatogram ids that should be written to mzML
-    if chromatogramIds is None and specfile in msrunContainer.cic:
-        chromatogramIds = [cId for cId in viewkeys(msrunContainer.cic[specfile])]
-    chromatogramCounts = len(chromatogramIds)
-
-    xmlFile = ETREE.xmlfile(outputFile, encoding='ISO-8859-1', buffered=False)
-    xmlWriter = xmlFile.__enter__()
-    xmlWriter.write_declaration()
-
-    nsmap = {None: 'http://psi.hupo.org/ms/mzml', 'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
-    xmlRoot = xmlWriter.element(metadataTree.tag, metadataTree.attrib, nsmap=nsmap)
-    xmlRoot.__enter__()
-    xmlWriter.write('\n')
-
-    for metadataNode in metadataTree.getchildren():
-        if metadataNode.tag == 'run':
-            xmlRun = xmlWriter.element(metadataNode.tag, metadataNode.attrib)
-            xmlRun.__enter__()
-            xmlWriter.write('\n')
-            for runChild in metadataNode.getchildren():
-                if runChild.tag == 'spectrumList':
-                    specDefaultProcRef = runChild.attrib['defaultDataProcessingRef']
-                elif runChild.tag == 'chromatogramList':
-                    chromDefaultProcRef = runChild.attrib['defaultDataProcessingRef']
-                else:
-                    #TODO: maybe recCopy?
-                    xmlRun.append(runChild)
-
-            #If any spectra should be written, generate the spectrumList Node
-            if spectrumCounts > 0:
-                specListAttribs = {'count': str(spectrumCounts),
-                                   'defaultDataProcessingRef': specDefaultProcRef}
-                xmlSpectrumList = xmlWriter.element('spectrumList', specListAttribs)
-                xmlSpectrumList.__enter__()
-                xmlWriter.write('\n')
-
-                for index, key in enumerate(spectrumIds):
-                    #TODO: proper container format instead of msrunContainer
-                    smi = msrunContainer.smic[specfile][key]
-                    sai = msrunContainer.saic[specfile][key]
-                    xmlSpectrum = xmlSpectrumFromSmi(index, smi, sai)
-                    xmlWriter.write(xmlSpectrum, pretty_print=True)
-                xmlSpectrumList.__exit__(None, None, None)
-                xmlWriter.write('\n')
-
-            #If any chromatograms should be written, generate the chromatogramList Node
-            if chromatogramCounts > 0:
-                chromListAttribs = {'count': str(chromatogramCounts),
-                                    'defaultDataProcessingRef': chromDefaultProcRef}
-                xmlChromatogramList = xmlWriter.element('chromatogramList', chromListAttribs)
-                xmlChromatogramList.__enter__()
-                xmlWriter.write('\n')
-                for index, key in enumerate(chromatogramIds):
-                    #TODO: proper container format instead of msrunContainer
-                    ci = msrunContainer.cic[specfile][key]
-                    xmlChromatogram = xmlChromatogramFromCi(index, ci)
-                    xmlWriter.write(xmlChromatogram, pretty_print=True)
-                xmlChromatogramList.__exit__(None, None, None)
-                xmlWriter.write('\n')
-
-            xmlRun.__exit__(None, None, None)
-            xmlWriter.write('\n')
-        else:
-            xmlWriter.write(maspy.xml.recCopyElement(metadataNode), pretty_print=True)
-    # close
-    xmlRoot.__exit__(None, None, None)
-    xmlFile.__exit__(None, None, None)
-
-    filepath = aux.joinpath(outputdir, specfile+'.mzML')
-    with open(filepath, 'wb') as openfile:
-        openfile.write(outputFile.getvalue())
-
-
-##########################################################
 # --- generate python objects from mzML xml elements --- #
-##########################################################
 def ciFromXml(xmlelement):
+    #TODO: docstring
     ci = maspy.core.Ci()
     for key in ['id', 'dataProcessingRef']:
         if key in xmlelement.attrib:
@@ -198,6 +113,7 @@ def ciFromXml(xmlelement):
 
 
 def smiFromXmlSpectrum(xmlelement, specfile):
+    #TODO: docstring
     scanId = xmlelement.attrib['id'].split('scan=')[1] #TODO: change to use regex to extract from known vendor format
     smi = maspy.core.Smi(scanId, specfile)
     smi.params, spectrumChildren = maspy.xml.extractParams(xmlelement)
@@ -226,10 +142,9 @@ def smiFromXmlSpectrum(xmlelement, specfile):
     return smi, binaryDataArrayList
 
 
-####################################################################################
 # --- extract spectrum attributes from the mzml like structure of a Smi object --- #
-####################################################################################
 def fetchSpectrumInfo(smi):
+    #TODO: docstring
     attributes = dict()
     for key, accession, dtype in [('msLevel', 'MS:1000511', int),
                                   ('tic', 'MS:1000285', float),
@@ -246,6 +161,7 @@ def fetchSpectrumInfo(smi):
 
 
 def fetchScanInfo(smi):
+    #TODO: docstring
     if smi.scanList:
         attributes = dict()
         for key, accession, dtype in [('rt', 'MS:1000016', float),
@@ -266,6 +182,7 @@ def fetchScanInfo(smi):
 
 
 def fetchParentIon(smi):
+    #TODO: docstring
     if smi.precursorList:
         attributes = dict()
         attributes['precursorId'] = smi.precursorList[0].spectrumRef
@@ -286,7 +203,7 @@ def fetchParentIon(smi):
 
 def defaultFetchSiAttrFromSmi(smi, si):
     """Default method to extract attributes from a spectrum metadata item (sai) and
-    add them to a spectrum item (si)."""
+    adding them to a spectrum item (si)."""
     for key, value in viewitems(fetchSpectrumInfo(smi)):
         setattr(si, key, value)
     for key, value in viewitems(fetchScanInfo(smi)):
@@ -296,185 +213,9 @@ def defaultFetchSiAttrFromSmi(smi, si):
             setattr(si, key, value)
 
 
-#####################################################
-# --- generate mzml elements from maspy objects --- #
-#####################################################
-def xmlGenScanList(scanList, scanListParams):
-    numEntries = len(scanList)
-    xmlScanList = ETREE.Element('scanList', {'count': str(numEntries)})
-    maspy.xml.xmlAddParams(xmlScanList, scanListParams)
-    for scan in scanList:
-        #Note: no attributes supported
-        xmlScan = ETREE.Element('scan', {})
-        maspy.xml.xmlAddParams(xmlScan, scan['params'])
-
-        #Generate the scanWindowList entry
-        numScanWindows = len(scan['scanWindowList'])
-        if numScanWindows > 0:
-            xmlScanWindowList = ETREE.Element('scanWindowList', {'count': str(numScanWindows)})
-            for scanWindow in scan['scanWindowList']:
-                xmlScanWindow = ETREE.Element('scanWindow')
-                maspy.xml.xmlAddParams(xmlScanWindow, scanWindow)
-                xmlScanWindowList.append(xmlScanWindow)
-            xmlScan.append(xmlScanWindowList)
-
-        xmlScanList.append(xmlScan)
-    return xmlScanList
-
-
-def xmlGenPrecursorList(precursorList):
-    numEntries = len(precursorList)
-    xmlPrecursorList = ETREE.Element('precursorList', {'count': str(numEntries)})
-    for precursor in precursorList:
-        #Note: no attributes supported
-        precursorAttrib = {}
-        if precursor['spectrumRef'] is not None:
-            precursorAttrib.update({'spectrumRef': precursor['spectrumRef']})
-        xmlPrecursor = ETREE.Element('precursor', precursorAttrib)
-
-        xmlActivation = ETREE.Element('activation')
-        maspy.xml.xmlAddParams(xmlActivation, precursor['activation'])
-        xmlPrecursor.append(xmlActivation)
-
-        if precursor['isolationWindow'] is not None:
-            xmlIsolationWindow = ETREE.Element('isolationWindow')
-            maspy.xml.xmlAddParams(xmlIsolationWindow, precursor['isolationWindow'])
-            xmlPrecursor.append(xmlIsolationWindow)
-
-        #Generate the selectedIonList entry
-        numSelectedIons = len(precursor['selectedIonList'])
-        if numSelectedIons > 0:
-            xmlSelectedIonList = ETREE.Element('selectedIonList', {'count': str(numSelectedIons)})
-            for selectedIon in precursor['selectedIonList']:
-                xmlSelectedIon = ETREE.Element('selectedIon')
-                maspy.xml.xmlAddParams(xmlSelectedIon, selectedIon)
-                xmlSelectedIonList.append(xmlSelectedIon)
-            xmlPrecursor.append(xmlSelectedIonList)
-
-        xmlPrecursorList.append(xmlPrecursor)
-    return xmlPrecursorList
-
-
-def xmlGenProductList(productList):
-    #TODO
-    pass
-
-
-def xmlGenBinaryDataArrayList(binaryDataInfo, binaryDataDict, compression='zlib', arrayTypes=None):
-    #compression: any other value than 'zlib' results in no compression
-    #Use datatypes parameter to specify the order of the arrays
-    arrayTypes = [_ for _ in viewkeys(binaryDataInfo)] if arrayTypes is None else arrayTypes
-    numEntries = len(binaryDataInfo)
-    xmlBinaryDataArrayList = ETREE.Element('binaryDataArrayList', {'count': str(numEntries)})
-    for arrayType in arrayTypes:
-        _, dataTypeParam = maspy.xml.findBinaryDataType(binaryDataInfo[arrayType]['params'])
-        binaryData = binaryDataDict[arrayType]
-        bitEncoding = '64' if binaryData.dtype.str == '<f8' else '32'
-        binaryData, arrayLength = maspy.xml.encodeBinaryData(binaryData, bitEncoding, compression)
-
-        # --- define binaryDataArray parameters --- #
-        params = list()
-        if bitEncoding == '64':
-            params.append(('MS:1000523', None, None))
-        else:
-            params.append(('MS:1000521', None, None))
-        if compression == 'zlib':
-            params.append(('MS:1000574', None, None))
-        else:
-            params.append(('MS:1000576', None, None))
-        mandatoryAccessions = ['MS:1000523', 'MS:1000521', 'MS:1000574', 'MS:1000576']
-        for param in binaryDataInfo[arrayType]['params']:
-            if param[0] not in mandatoryAccessions:
-                params.append(param)
-
-        #Note: not all attributes supported
-        binaryDataArrayAttrib = {'encodedLength': str(len(binaryData))}
-        for attr in ['dataProcessingRef']:
-            if binaryDataInfo[arrayType][attr] is not None:
-                binaryDataArrayAttrib[attr] = binaryDataInfo[arrayType][attr]
-        xmlBinaryDataArray = ETREE.Element('binaryDataArray', binaryDataArrayAttrib)
-        maspy.xml.xmlAddParams(xmlBinaryDataArray, params)
-
-        xmlBinary = ETREE.Element('binary')
-        xmlBinary.text = binaryData
-        xmlBinaryDataArray.append(xmlBinary)
-        xmlBinaryDataArrayList.append(xmlBinaryDataArray)
-    return xmlBinaryDataArrayList
-
-
-def xmlSpectrumFromSmi(index, smi, sai=None, compression='zlib'):
-    """
-    index: The zero-based, consecutive index of the spectrum in the SpectrumList. (mzML specification)
-    smi: a SpectrumMetadataItem instance
-    sai: a SpectrumArrayItem instance, if None no binaryDataArrayList is written
-    """
-    if sai is not None:
-        arrayLength = [array.size for array in viewvalues(sai.arrays)]
-        if len(set(arrayLength)) != 1:
-            raise Exception('Unequal array size for different entries in sai.arrays')
-        else:
-            arrayLength = arrayLength[0]
-    else:
-        arrayLength = 0
-
-    spectrumAttrib = {'index': str(index), 'id': smi.attributes['id'],
-                      'defaultArrayLength': str(arrayLength)}
-
-    xmlSpectrum = ETREE.Element('spectrum', **spectrumAttrib)
-    maspy.xml.xmlAddParams(xmlSpectrum, smi.params)
-    #Add the scanList
-    if len(smi.scanList) > 0:
-        xmlSpectrum.append(xmlGenScanList(smi.scanList, smi.scanListParams))
-    if len(smi.precursorList) > 0:
-        xmlSpectrum.append(xmlGenPrecursorList(smi.precursorList))
-    if len(smi.productList) > 0:
-        xmlSpectrum.append(xmlGenProductList(smi.productList))
-    if sai is not None:
-        xmlSpectrum.append(xmlGenBinaryDataArrayList(sai.arrayInfo,
-                                                     sai.arrays,
-                                                     compression=compression
-                                                     ))
-    return xmlSpectrum
-
-
-def xmlChromatogramFromCi(index, ci, compression='zlib'):
-    arrayLength = [array.size for array in viewvalues(ci.arrays)]
-    if len(set(arrayLength)) != 1:
-        raise Exception('Unequal array size for different entries in sai.arrays')
-    else:
-        arrayLength = arrayLength[0]
-
-    chromatogramAttrib = {'index': str(index), 'id': ci.id,
-                          'defaultArrayLength': str(arrayLength)}
-    if 'dataProcessingRef' in ci.attrib:
-        chromatogramAttrib.update({'dataProcessingRef': dataProcessingRef})
-
-    xmlChromatogram = ETREE.Element('chromatogram', **chromatogramAttrib)
-    maspy.xml.xmlAddParams(xmlChromatogram, ci.params)
-    #TODO: add appropriate functions for precursor and product
-    if ci.product is not None:
-        pass
-    if ci.precursor is not None:
-        pass
-
-    #Sort the array keys, that 'rt' is always the first, necessary for the software "SeeMS" to properly display chromatograms
-    arrayTypes = set(ci.arrayInfo)
-    if 'rt' in arrayTypes:
-        arrayTypes.remove('rt')
-        arrayTypes = ['rt'] + list(arrayTypes)
-    else:
-        arrayTypes = list(arrayTypes)
-
-    xmlChromatogram.append(xmlGenBinaryDataArrayList(ci.arrayInfo,
-                                                     ci.arrays,
-                                                     compression=compression,
-                                                     arrayTypes=arrayTypes
-                                                     )
-                           )
-    return xmlChromatogram
-
-
-# --- import for SiiContainer class --- #
+#####################################################################
+### import for SiiContainer class             #######################
+#####################################################################
 def importPsmResults(siiContainer, fileLocation, specfile, psmType='percolator', psmEngine='comet', qValue=0.01):
     """Function to control the import of PSM results into :class:`SiiContainer`.
 
@@ -507,7 +248,7 @@ def _importPercolatorResults(fileLocation, psmEngine=None):
 
     See also :func:`importPsmResults` and :func:`_importFromPercolatorArray`
     """
-    #HEADERLINE: xtandem seperates proteins with ';', msgf separates proteins by a tab
+    #Note: regarding headerline, xtandem seperates proteins with ';', msgf separates proteins by a tab
     with io.open(fileLocation, 'r', encoding='utf-8') as openfile:
         lines = openfile.readlines()
         headerDict = dict([[y,x] for (x,y) in enumerate(lines[0].strip().split('\t'))])
@@ -609,7 +350,9 @@ def _importFromPercolatorArrays(siiContainer, psmArrays, specfile, qValueCutOff=
         siiContainer.container[specfile][sii.id].append(sii)
 
 
-# --- import for FeatureContainer class --- #
+#####################################################################
+### import for FeatureContainer class         #######################
+#####################################################################
 def importPeptideFeatures(fiContainer, filelocation, specfile):
     """ Import peptide features from a featureXml file (eg. generated by OPENMS featureFinderCentroided).
 
